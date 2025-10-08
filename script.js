633a403c9e76a0a8d6dc62e9a64e7807d8de5ed2
j-dio <tcshs.lumacang.johndio.a@gmail.com>
2025-10-08 19:21:26 +0800

    Added js file for modal to appear when floating cards are clicked

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function toggleHamburger() {
-  const menu = document.getElementById("mobileMenu").classList.toggle("active");
-}
+// wait for page to fully load
+document.addEventListener("DOMContentLoaded", () => {
+  // get references to modal and class button
+  const modal = document.getElementById("cardModal");
+  const closeBtn = document.querySelector(".close-btn");
+
+  // get all floating cards
+  const floatingCards = document.querySelectorAll(".floating-card");
+
+  // for each card, add a click event
+  floatingCards.forEach((card) => {
+    card.addEventListener("click", () => {
+      modal.style.display = "flex"; // show modal
+    });
+  });
+
+  // close modal when x button is clicked
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  })
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+})
